6572a1219747e4c175dbc799561c3dda4d4319f2
bakhtiarabid <abid.bu16@gmail.com>
2021-03-21 22:35:38 +0600

    Karel

---
+++ Karel.js
@@ -0,0 +1,31 @@
+function main(){
+   //your code here
+  putBeeper();
+move();
+turnLeft();
+move();
+  putBeeper();
+  
+goAgain();
+
+}
+   
+    
+
+
+function goAgain(){
+   move();
+turnRight();
+move();
+putBeeper();
+
+
+   move();
+   turnLeft();
+   move();
+   putBeeper();
+   move();
+   turnRight();
+   move();
+   putBeeper();
+}
